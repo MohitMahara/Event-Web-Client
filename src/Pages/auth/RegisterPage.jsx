b951332e3c701c0552dadf41378987e4712c418f
Mohit Mahara <112603296+MohitMahara@users.noreply.github.com>
2025-04-16 09:28:36 +0530

    Update RegisterPage.jsx

--- src/Pages/auth/RegisterPage.jsx
+++ src/Pages/auth/RegisterPage.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-import { AuthHeader } from "./authHeader";
+import { AuthHeader } from "./AuthHeader";
 import { useNavigate } from "react-router-dom";
 
 export const RegisterPage = () => {
