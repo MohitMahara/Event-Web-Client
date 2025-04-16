a25c7519b7bb33c99c8094610c34752ffb93f120
Mohit Mahara <112603296+MohitMahara@users.noreply.github.com>
2025-04-16 20:51:42 +0530

    event page work in progress

--- src/Pages/Event/EventPage.jsx
+++ src/Pages/Event/EventPage.jsx
@@ -28,6 +28,16 @@ export const EventPage = () =>{
     }, [])
 
 
+    const handleRegister = async() =>{
+      try {
+        
+      } catch (error) {
+        
+      }
+    }
+
+
+
     //  function to get event data from db
 
 
@@ -44,10 +54,31 @@ export const EventPage = () =>{
     return (
         <>
          <Layout>
-            <div className="w-full md:m-5xl mx-auto px-4 py-4">
-               <h2 className="text-4xl text-gray-900 font-bold text-center">{event[0].title}</h2>
+            <div className="w-full md:w-6xl mx-auto px-4 py-8 min-h-screen">
+               <h2 className="text-4xl text-gray-900 font-bold text-center">{event[0]?.title}</h2>
 
+              <div className="flex flex-row w-full min-h-screen my-8 gap-4">
+                 <div className="w-3/4 h-full">
+                    <img src={event[0]?.image} alt={event[0]?.title} className="w-full h-100 object-cover rounded-lg" />
+                    <p className="text-gray-700 text-lg mt-4">{event[0]?.description}</p>
+                 </div>
+                <div className="w-1/4 h-100 bg-white  p-4 rounded-lg shadow-lg">
+                    <h3 className="text-xl text-gray-800  text-center font-bold">Event Details</h3>
+                     <p className="text-gray-700 text-lg mt-2">Happening on</p>
+                     <p className="text-gray-900 text-md mt-2 font-bold">{event[0]?.date}</p>
+                     <p className="text-gray-700 text-lg mt-2">Venue</p>
+                     <p className="text-gray-900 text-md mt-2 font-bold">{event[0]?.venue}</p>
+                     <p className="text-gray-700 text-lg mt-2">Organizer</p>
+                     <p className="text-gray-900 text-md mt-2 font-bold">{event[0]?.organizer}</p>
 
+                     {new Date(event[0]?.date).getTime() > Date.now() ?
+                      <button className="bg-blue-500 px-4 py-2 rounded-lg text-gray-100 w-full mt-4 text-lg cursor-pointer" onClick={handleRegister}>Register</button>
+                       :
+                      <button className="bg-blue-500 px-4 py-2 rounded-lg text-gray-100 w-full mt-4 text-lg">Ended</button>
+                     }
+                </div>
+
+              </div>
 
 
             </div>
