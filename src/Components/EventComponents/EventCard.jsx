090653bf4c0a88ac53d69b34921844d2894e7833
Mohit Mahara <112603296+MohitMahara@users.noreply.github.com>
2025-04-19 23:57:35 +0530

    organizer dashboard : work in progress

--- src/Components/EventComponents/EventCard.jsx
+++ src/Components/EventComponents/EventCard.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import { FaLocationDot } from "react-icons/fa6";
-import { Link } from "react-router-dom";
 
 
 export const EventCard = ({ event }) => {
